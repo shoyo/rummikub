1073fb8f139cbb1e00f04ec08615c50e65ca86cf
Shoyo Inokuchi <contact@shoyo.dev>
2020-11-26 15:28:23 -0800

    Add more unit tests, and begin implementing tile parser

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 use std::collections::HashMap;
 
 /// A single Rummikub tile
+#[derive(PartialEq)]
 enum Tile {
     Basic(BasicTile),
     Joker(Joker),
 }
 
+#[derive(PartialEq)]
 struct BasicTile {
     color: TileColor,
     value: TileValue,
@@ -21,7 +23,7 @@ impl BasicTile {
     }
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
 enum TileColor {
     Black,
     Red,
@@ -40,6 +42,7 @@ fn format_color(color_code: TileColor) -> String {
     }
 }
 
+#[derive(PartialEq)]
 struct Joker {
     variant: JokerVariant,
 }
@@ -50,6 +53,7 @@ impl Joker {
     }
 }
 
+#[derive(PartialEq)]
 enum JokerVariant {
     Single,
     Double,
@@ -72,44 +76,167 @@ enum Parsing {
         color: TileColor,
     },
     Group {
+        value: TileValue,
         seen: HashMap<TileColor, bool>,
+        size: u8,
     },
-    Undetermined,
+    Undetermined {
+        tile_seen: Option<(TileValue, TileColor)>,
+        size: u8,
+    },
 }
 
-/// Given an ordered sequence of Rummikub tiles, return whether the sequence is valid.
+/// Given an ordered set of Rummikub tiles, return whether the set is valid.
 fn is_valid_set(set: Vec<Tile>) -> bool {
     if set.len() < 3 {
         return false;
     }
-    let mut parsing = Parsing::Undetermined;
-
+    let mut parsing = Parsing::Undetermined {
+        tile_seen: None,
+        size: 0,
+    };
     let mut tiles = set.iter().enumerate();
+
     while let Some((index, tile)) = tiles.next() {
         match parsing {
             Parsing::Run {
-                ref last_value,
-                ref color,
+                ref mut last_value,
+                ref mut color,
             } => match tile {
                 Tile::Basic(t) => {
-                    if t.value == 0 || t.value > 13 {
-                        panic!(
-                            "Illegal tile value {}: tiles should be 0 < value <= 13",
-                            t.value
-                        );
-                    }
-                    if t.value != last_value + 1 {
+                    _assert_valid_tile_value(t.value);
+                    if t.value != *last_value + 1 {
                         return false;
                     }
                     if t.color != *color {
                         return false;
                     }
+                    *last_value += 1;
                 }
-                Tile::Joker(j) => {}
+                Tile::Joker(j) => match j.variant {
+                    JokerVariant::Single => {
+                        *last_value += 1;
+                    }
+                    JokerVariant::Double => {
+                        *last_value += 2;
+                    }
+                    JokerVariant::Mirror => {
+                        return _handle_mirror_joker(&set, index);
+                    }
+                    JokerVariant::ColorChange => {}
+                },
             },
-            Parsing::Group { ref mut seen } => {}
-            Parsing::Undetermined => {}
+            Parsing::Group {
+                ref mut value,
+                ref mut seen,
+                ref mut size,
+            } => match tile {
+                Tile::Basic(t) => {
+                    _assert_valid_tile_value(t.value);
+                }
+                Tile::Joker(j) => match j.variant {
+                    JokerVariant::Single => {
+                        *size += 1;
+                    }
+                    JokerVariant::Double => {
+                        let num_seen = seen
+                            .iter()
+                            .filter(|(_, v)| **v)
+                            .collect::<Vec<(&TileColor, &bool)>>()
+                            .len();
+                        if num_seen > 2 {
+                            return false;
+                        }
+                        *size += 2;
+                    }
+                    JokerVariant::Mirror => {
+                        return _handle_mirror_joker(&set, index);
+                    }
+                    JokerVariant::ColorChange => {
+                        return false;
+                    }
+                },
+            },
+            Parsing::Undetermined {
+                ref mut tile_seen,
+                ref mut size,
+            } => match tile {
+                Tile::Basic(t) => {
+                    _assert_valid_tile_value(t.value);
+                    match *tile_seen {
+                        Some((value, color)) => {
+                            if t.value == value + 1 && t.color == color {
+                                parsing = Parsing::Run {
+                                    last_value: t.value,
+                                    color: color,
+                                };
+                            } else if t.value == value && t.color != color {
+                                let mut seen = HashMap::new();
+                                seen.insert(TileColor::Black, false);
+                                seen.insert(TileColor::Red, false);
+                                seen.insert(TileColor::Blue, false);
+                                seen.insert(TileColor::Orange, false);
+                                seen.insert(t.color, true);
+                                seen.insert(color, true);
+                                parsing = Parsing::Group {
+                                    value: value,
+                                    seen: seen,
+                                    size: *size + 1,
+                                };
+                            } else {
+                                return false;
+                            }
+                        }
+                        None => {
+                            *tile_seen = Some((t.value, t.color));
+                        }
+                    }
+                }
+                Tile::Joker(j) => match j.variant {
+                    JokerVariant::Single => {
+                        *size + 1;
+                    }
+                    JokerVariant::Double => {
+                        *size + 2;
+                    }
+                    JokerVariant::Mirror => {
+                        _handle_mirror_joker(&set, index);
+                    }
+                    JokerVariant::ColorChange => match tile_seen {
+                        Some((value, color)) => {}
+                        None => {}
+                    },
+                },
+            },
         }
+    }
+    return true;
+}
+
+fn _assert_valid_tile_value(value: TileValue) {
+    if value == 0 || value > 13 {
+        panic!(
+            "Illegal tile value {}: tiles should be 0 < value <= 13",
+            value
+        );
+    }
+}
+
+fn _handle_mirror_joker(set: &Vec<Tile>, mirror_index: usize) -> bool {
+    if mirror_index == 0 {
+        return false;
+    }
+    let mut left = mirror_index - 1;
+    let mut right = mirror_index + 1;
+    while left >= 0 && right < set.len() {
+        if left == 0 && right != set.len() - 1 || left != 0 && right == set.len() {
+            return false;
+        }
+        if set[left] != set[right] {
+            return false;
+        }
+        left -= 1;
+        right += 1;
     }
     return true;
 }
@@ -242,6 +369,17 @@ mod tests {
     }
 
     #[test]
+    fn test_invalid_run_with_single_joker_2() {
+        let set = vec![
+            Tile::Joker(Joker::new(JokerVariant::Single)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 1)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 2)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 3)),
+        ];
+        assert_eq!(is_valid_set(set), false);
+    }
+
+    #[test]
     fn test_valid_group_with_single_joker() {
         let set = vec![
             Tile::Basic(BasicTile::new(TileColor::Red, 8)),
@@ -324,6 +462,17 @@ mod tests {
         assert_eq!(is_valid_set(set), false);
     }
 
+    #[test]
+    fn test_invalid_group_with_double_joker() {
+        let set = vec![
+            Tile::Basic(BasicTile::new(TileColor::Red, 8)),
+            Tile::Basic(BasicTile::new(TileColor::Blue, 8)),
+            Tile::Joker(Joker::new(JokerVariant::Double)),
+            Tile::Basic(BasicTile::new(TileColor::Black, 8)),
+        ];
+        assert_eq!(is_valid_set(set), false);
+    }
+
     // MIRROR JOKER
 
     #[test]
@@ -375,6 +524,19 @@ mod tests {
     }
 
     #[test]
+    fn test_invalid_run_with_mirror_2() {
+        let set = vec![
+            Tile::Basic(BasicTile::new(TileColor::Red, 7)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 8)),
+            Tile::Joker(Joker::new(JokerVariant::Mirror)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 8)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 7)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 6)),
+        ];
+        assert_eq!(is_valid_set(set), false);
+    }
+
+    #[test]
     fn test_invalid_run_with_two_mirrors() {
         let set = vec![
             Tile::Basic(BasicTile::new(TileColor::Red, 8)),
@@ -393,7 +555,7 @@ mod tests {
         let set = vec![
             Tile::Basic(BasicTile::new(TileColor::Red, 6)),
             Tile::Basic(BasicTile::new(TileColor::Red, 7)),
-            Tile::Joker(Joker::new(JokerVariant::Mirror)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
             Tile::Basic(BasicTile::new(TileColor::Blue, 9)),
             Tile::Basic(BasicTile::new(TileColor::Blue, 10)),
         ];
@@ -405,7 +567,7 @@ mod tests {
         let set = vec![
             Tile::Basic(BasicTile::new(TileColor::Red, 6)),
             Tile::Basic(BasicTile::new(TileColor::Red, 7)),
-            Tile::Joker(Joker::new(JokerVariant::Mirror)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
             Tile::Basic(BasicTile::new(TileColor::Red, 9)),
             Tile::Basic(BasicTile::new(TileColor::Red, 10)),
         ];
@@ -417,11 +579,65 @@ mod tests {
         let set = vec![
             Tile::Basic(BasicTile::new(TileColor::Red, 6)),
             Tile::Basic(BasicTile::new(TileColor::Red, 7)),
-            Tile::Joker(Joker::new(JokerVariant::Mirror)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
             Tile::Basic(BasicTile::new(TileColor::Red, 8)),
             Tile::Basic(BasicTile::new(TileColor::Red, 9)),
         ];
         assert_eq!(is_valid_set(set), false);
+    }
+
+    #[test]
+    fn test_valid_run_with_two_color_changes() {
+        let set = vec![
+            Tile::Basic(BasicTile::new(TileColor::Orange, 6)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 8)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+            Tile::Basic(BasicTile::new(TileColor::Blue, 10)),
+        ];
+        assert_eq!(is_valid_set(set), true);
+    }
+
+    #[test]
+    fn test_valid_run_with_two_color_change_sandwich() {
+        let set = vec![
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+            Tile::Basic(BasicTile::new(TileColor::Red, 8)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+        ];
+        assert_eq!(is_valid_set(set), true);
+    }
+
+    #[test]
+    fn test_valid_run_with_two_color_change_adjacent() {
+        let set = vec![
+            Tile::Basic(BasicTile::new(TileColor::Orange, 6)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+        ];
+        assert_eq!(is_valid_set(set), true);
+    }
+
+    #[test]
+    fn test_adjacent_color_change_house_rule() {
+        let set = vec![
+            Tile::Basic(BasicTile::new(TileColor::Orange, 6)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+            Tile::Basic(BasicTile::new(TileColor::Blue, 9)),
+        ];
+        assert_eq!(is_valid_set(set), true);
+    }
+
+    #[test]
+    fn test_adjacent_color_change_house_rule_2() {
+        let set = vec![
+            Tile::Basic(BasicTile::new(TileColor::Orange, 6)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+            Tile::Joker(Joker::new(JokerVariant::ColorChange)),
+            Tile::Basic(BasicTile::new(TileColor::Orange, 9)),
+        ];
+        assert_eq!(is_valid_set(set), true);
     }
 
     // MIXED JOKERS
